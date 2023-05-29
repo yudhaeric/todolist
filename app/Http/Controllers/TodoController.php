886635f15f8afef44b992aa37a25e8c729f9252d
yudhaeric <yudhaericpamungkas@gmail.com>
2023-05-29 17:35:12 +0700

    Update Todos

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -8,17 +8,38 @@ use App\Models\Todo;
 class TodoController extends Controller
 {
     public function index() {
-        $todo = Todo::all();
+        // Get Data 
+        $all = Todo::get();
+        $active = Todo::where('status', 1)->get();
+        $done = Todo::where('status', 0)->get();
 
+        // Get Amount of Data
+        $allAmount = Todo::count();
+        $activeAmount = Todo::where('status', 1)->count();
+        $doneAmount = Todo::where('status', 0)->count();
+
+        // Get Local Time
         date_default_timezone_set('Asia/Jakarta');
         $day = date('l');
         $date = date('d');
         $month = date('F');
 
-        return view('home', ['todolist' => $todo, 'day' => $day,'date' => $date, 'month' => $month]);
+        $data = [
+            'allTodos' => $all,
+            'activeTodos' => $active,
+            'doneTodos' => $done,
+            'allAmount' => $allAmount,
+            'activeAmount' => $activeAmount,
+            'doneAmount' => $doneAmount,
+            'day' => $day,
+            'date' => $date,
+            'month' => $month
+        ];
+        
+        return view('home', $data);
     }
 
-    public function create() {
-        return view('home-add');
-    }
+    // public function create() {
+    //     return view('home-add');
+    // }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,15 +19,15 @@
     <div class="flex items-center text-[#8A8A8A] text-sm justify-between">
       <button onclick="updateStatus('all')" class="flex items-center">
         <p class="mr-2 category-style">All</p>
-        <p class="amount-style px-[6px] bg-[#D9D9D9] text-white rounded-xl text-[12px]">1</p>
+        <p class="amount-style px-[6px] bg-[#D9D9D9] text-white rounded-xl text-[12px]">{{$allAmount}}</p>
       </button>
       <button onclick="updateStatus('active')" class="flex items-center">
         <p class="mr-2 category-style">Active</p>
-        <p class="amount-style px-[6px] bg-[#D9D9D9] text-white rounded-xl text-[12px]">1</p>
+        <p class="amount-style px-[6px] bg-[#D9D9D9] text-white rounded-xl text-[12px]">{{$activeAmount}}</p>
       </button>
       <button onclick="updateStatus('done')" class="flex items-center">
         <p class="mr-2 category-style">Done</p>
-        <p class="amount-style px-[6px] bg-[#D9D9D9] text-white rounded-xl text-[12px]">0</p>
+        <p class="amount-style px-[6px] bg-[#D9D9D9] text-white rounded-xl text-[12px]">{{$doneAmount}}</p>
       </button>
       <button onclick="updateStatus('delete')">
         <p class="mr-1 category-style">Deleted</p>
@@ -36,7 +36,7 @@
   </div>
   <div class="flex flex-col items-center justify-center mt-4">
     <div id="all-todo-list" class="content">
-      @foreach ($todolist as $item)
+      @foreach ($allTodos as $item)
         <div class="w-80 h-28 bg-[#FFFFFF] rounded-2xl mb-5 px-5 py-3">
           <div class="grid grid-cols-10 gap-2">
             <div class="col-span-9 overflow-hidden">
@@ -52,10 +52,36 @@
       @endforeach
     </div>
     <div id="active-todo-list" class="content">
-      <h1>Todos Active</h1>
+      @foreach ($activeTodos as $item)
+        <div class="w-80 h-28 bg-[#FFFFFF] rounded-2xl mb-5 px-5 py-3">
+          <div class="grid grid-cols-10 gap-2">
+            <div class="col-span-9 overflow-hidden">
+              <h1 class="mb-1 font-bold">{{$item->title}}</h1>
+              <p class="text-xs text-[#8A8A8A] w-[95%]">{{$item->subtitle}}</p>
+            </div>
+            <div class="flex items-center h-[90%]">
+              <div class="border border-[#8A8A8A] w-[18px] h-[18px] rounded-full"></div>
+            </div>
+          </div>
+          <p class="text-[10px] text-[#8A8A8A] mt-3 border-t-2 pt-3 font-bold">April 27</p>
+        </div>
+      @endforeach
     </div>
     <div id="done-todo-list" class="content">
-      <h1>Todos Done</h1>
+      @foreach ($doneTodos as $item)
+        <div class="w-80 h-28 bg-[#FFFFFF] rounded-2xl mb-5 px-5 py-3">
+          <div class="grid grid-cols-10 gap-2">
+            <div class="col-span-9 overflow-hidden">
+              <h1 class="mb-1 font-bold">{{$item->title}}</h1>
+              <p class="text-xs text-[#8A8A8A] w-[95%]">{{$item->subtitle}}</p>
+            </div>
+            <div class="flex items-center h-[90%]">
+              <div class="border border-[#8A8A8A] w-[18px] h-[18px] rounded-full"></div>
+            </div>
+          </div>
+          <p class="text-[10px] text-[#8A8A8A] mt-3 border-t-2 pt-3 font-bold">April 27</p>
+        </div>
+      @endforeach
     </div>
     <div id="delete-todo-list" class="content">
       <h1>Todos Deleted</h1>
