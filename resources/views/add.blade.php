9ca7c5c79daa87954ebd12fde211c3884d0e07f2
yudhaeric <yudhaericpamungkas@gmail.com>
2023-06-02 14:34:43 +0700

    Fix Issue Input Task was not Entered to Database

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -18,8 +18,8 @@
                 <input required type="text" name="title" id="title" placeholder="Add Task Name..." class="text-[12px] outline-[#131313] mt-2 mx-8 bg-[#EFF3F4] rounded-md py-2 pl-4">
             </div>
             <div class="flex flex-col overflow-hidden w-96">
-                <label for="description" class="mt-4 mx-8 font-bold text-[#131313]">Description</label>
-                <textarea name="description" id="description" rows="4" cols="50" placeholder="Add Descriptions..."  maxlength="100" class="resize-none overflow-hidden bg-[#EFF3F4] mt-2 mx-8 px-2 pt-2 pb-[80%] outline-[#131313] rounded-md text-[12px]"></textarea>
+                <label for="task" class="mt-4 mx-8 font-bold text-[#131313]">Description</label>
+                <textarea name="task" id="task" rows="4" cols="50" placeholder="Add Descriptions..."  maxlength="100" class="resize-none overflow-hidden bg-[#EFF3F4] mt-2 mx-8 px-2 pt-2 pb-[80%] outline-[#131313] rounded-md text-[12px]"></textarea>
             </div>
             <div class="flex justify-center gap-2 mt-5">
                 <a href="/" class="w-[155px] flex justify-center items-center h-12 border border-[#3085FE] rounded-lg text-[#3085FE] font-bold">
