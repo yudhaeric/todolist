3aa61ac02e417a555a702ba2628851bbefb72693
yudhaeric <yudhaericpamungkas@gmail.com>
2023-06-14 23:02:17 +0700

    Update Add Data Page

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -19,7 +19,7 @@
             </div>
             <div class="flex flex-col overflow-hidden w-96">
                 <label for="task" class="mt-4 mx-8 font-bold text-[#131313]">Description</label>
-                <textarea name="task" id="task" rows="4" cols="50" placeholder="Add Descriptions..."  maxlength="100" class="resize-none overflow-hidden bg-[#EFF3F4] mt-2 mx-8 px-2 pt-2 pb-[80%] outline-[#131313] rounded-md text-[12px]"></textarea>
+                <textarea name="task" id="task" rows="4" cols="50" placeholder="Add Descriptions..."  maxlength="100" class="resize-none overflow-hidden bg-[#EFF3F4] mt-2 mx-8 px-2 pt-2 pb-[60%] outline-[#131313] rounded-md text-[12px]"></textarea>
             </div>
             <div class="flex justify-center gap-2 mt-5">
                 <a href="/" class="w-[155px] flex justify-center items-center h-12 border border-[#3085FE] rounded-lg text-[#3085FE] font-bold">
